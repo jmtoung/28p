274ab6f8d62387fcb97e3eec491523f716c9afa4
jmtoung@gmail.com <jmtoung@gmail.com>
2016-05-16 22:30:07 -0700

    moved all files on master to v_angular branch

--- README.md
+++
@@ -1,2 +0,0 @@
-# 28p
-profit logger
--- src/app/core/constants.js
+++
@@ -1,8 +0,0 @@
-(function() {
-    'use strict';
-    
-    angular
-        .module('app.core', [])
-        .constant('FIREBASE_URL', 'https://theprofitlogger.firebaseio.com/');
-    
-})();
